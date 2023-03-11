fff0296b26e77f63d2a2560d07f345c03f5988ef
Bryan Escobar <bescobarres@gmail.com>
2023-03-11 14:44:57 +1030

    Objeto literal, clonacion de objeto

--- 06-destructuracion-datos/src/index.js
+++ 06-destructuracion-datos/src/index.js
@@ -9,3 +9,10 @@ const {name, surname} = user;
 console.log(user);
 
 console.log(name + ' '+ surname);
+
+const user2 = {...user};
+
+user2.name = 'Bryan 2'
+
+console.table(user);
+console.table(user2);
